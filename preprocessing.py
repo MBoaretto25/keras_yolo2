f2f7a0b7dfdad281f9ec8152b3c03565e594d0bc
Rodrigo Meira de Andrade <rodrigormda@hotmail.com>
2018-06-05 17:52:41 -0300

    heavier augmented data

--- preprocessing.py
+++ preprocessing.py
@@ -141,47 +141,54 @@ class BatchGenerator(Sequence):
         self.aug_pipe = iaa.Sequential(
             [
                 # apply the following augmenters to most images
-                #iaa.Fliplr(0.5), # horizontally flip 50% of all images
-                #iaa.Flipud(0.2), # vertically flip 20% of all images
-                #sometimes(iaa.Crop(percent=(0, 0.1))), # crop images by 0-10% of their height/width
+                iaa.Fliplr(0.5), # horizontally flip 50% of all images
+                iaa.Flipud(0.2), # vertically flip 20% of all images
                 sometimes(iaa.Affine(
-                    #scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, individually per axis
-                    #translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
-                    #rotate=(-5, 5), # rotate by -45 to +45 degrees
-                    #shear=(-5, 5), # shear by -16 to +16 degrees
-                    #order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
-                    #cval=(0, 255), # if mode is constant, use a cval between 0 and 255
-                    #mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+                    scale={"x": (0.6, 1), "y": (0.6, 1)}, # scale images to 60-100% of their size, individually per axis
+                    shear=(-16, 16), # shear by -16 to +16 degrees
+                    cval=(0, 255), # if mode is constant, use a cval between 0 and 255
                 )),
                 # execute 0 to 5 of the following (less important) augmenters per image
                 # don't execute all of them, as that would often be way too strong
                 iaa.SomeOf((0, 5),
                     [
-                        #sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
+                        sometimes(iaa.Superpixels(p_replace=(0, 0.4), n_segments=(20, 200))), # convert images into their superpixel representation
                         iaa.OneOf([
-                            iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
+                            iaa.GaussianBlur((0, 2.0)), # blur images with a sigma between 0 and 2.0
                             iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
-                            iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
+                            iaa.MedianBlur(k=(3, 7)), # blur image using local medians with kernel sizes between 3 and 7
                         ]),
                         iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
-                        #iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
-                        # search either for all edges or for directed edges
-                        #sometimes(iaa.OneOf([
-                        #    iaa.EdgeDetect(alpha=(0, 0.7)),
-                        #    iaa.DirectedEdgeDetect(alpha=(0, 0.7), direction=(0.0, 1.0)),
-                        #])),
+                        iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
+                        # search either for all edges or for directed edges,
+                        # blend the result with the original image using a blobby mask
+                        iaa.SimplexNoiseAlpha(iaa.OneOf([
+                            iaa.EdgeDetect(alpha=(0.5, 1.0)),
+                            iaa.DirectedEdgeDetect(alpha=(0.5, 1.0), direction=(0.0, 1.0)),
+                        ])),
                         iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5), # add gaussian noise to images
                         iaa.OneOf([
                             iaa.Dropout((0.01, 0.1), per_channel=0.5), # randomly remove up to 10% of the pixels
-                            #iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
+                            iaa.CoarseDropout((0.03, 0.15), size_percent=(0.1, 0.3), per_channel=0.2),
                         ]),
-                        #iaa.Invert(0.05, per_channel=True), # invert color channels
+                        iaa.Invert(0.05, per_channel=True), # invert color channels
                         iaa.Add((-10, 10), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
-                        iaa.Multiply((0.5, 1.5), per_channel=0.5), # change brightness of images (50-150% of original value)
+                        iaa.AddToHueAndSaturation((-20, 20)), # change hue and saturation
+                        # either change the brightness of the whole image (sometimes
+                        # per channel) or change the brightness of subareas
+                        iaa.OneOf([
+                            iaa.Multiply((0.5, 1.5), per_channel=0.5),
+                            iaa.FrequencyNoiseAlpha(
+                                exponent=(-4, 0),
+                                first=iaa.Multiply((0.5, 1.5), per_channel=True),
+                                second=iaa.ContrastNormalization((0.5, 2.0))
+                            )
+                        ]),
                         iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5), # improve or worsen the contrast
-                        #iaa.Grayscale(alpha=(0.0, 1.0)),
-                        #sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
-                        #sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))) # sometimes move parts of the image around
+                        iaa.Grayscale(alpha=(0.0, 1.0)),
+                        sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
+                        sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))), # sometimes move parts of the image around
+                        sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
                     ],
                     random_order=True
                 )
@@ -331,26 +338,36 @@ class BatchGenerator(Sequence):
         h = image.shape[0]
         w = image.shape[1]
         all_objs = copy.deepcopy(train_instance['object'])
-
-        if jitter:
-            ### scale the image
-            scale = np.random.uniform() / 10. + 1.
-            image = cv2.resize(image, (0,0), fx = scale, fy = scale)
-
-            ### translate the image
-            max_offx = (scale-1.) * w
-            max_offy = (scale-1.) * h
-            offx = int(np.random.uniform() * max_offx)
-            offy = int(np.random.uniform() * max_offy)
-            
-            image = image[offy : (offy + h), offx : (offx + w)]
-
-            ### flip the image
-            flip = np.random.binomial(1, .5)
-            if flip > 0.5: image = cv2.flip(image, 1)
-                
-            image = self.aug_pipe.augment_image(image)            
-            
+        keypoints = []
+        if jitter:   
+            for obj in all_objs:
+                xmin = obj['xmin']
+                ymin = obj['ymin']
+                xmax = obj['xmax']
+                ymax = obj['ymax']   
+                keypoints.append(ia.Keypoint(x=xmin,y=ymin))
+                keypoints.append(ia.Keypoint(x=xmax,y=ymin))
+                keypoints.append(ia.Keypoint(x=xmax,y=ymax))
+                keypoints.append(ia.Keypoint(x=xmin,y=ymax)) 
+            keypoints_on_image = ia.KeypointsOnImage(keypoints,shape=image.shape)  
+            seq_det = self.aug_pipe.to_deterministic()     
+            image = seq_det.augment_image(image) 
+            keypoints_aug = seq_det.augment_keypoints([keypoints_on_image])
+            coords = keypoints_aug[0].get_coords_array()  
+            for i in range(0,len(coords),4):
+                xa=coords[i,0]
+                ya=coords[i,1]
+                xb=coords[i+1,0]
+                yb=coords[i+1,1]
+                xc=coords[i+2,0]
+                yc=coords[i+2,1]
+                xd=coords[i+3,0]
+                yd=coords[i+3,1]
+                all_objs[i//4]['xmin'] = max(min(xa,xb,xc,xd),0)
+                all_objs[i//4]['xmax'] = min(max(xa,xb,xc,xd),image.shape[1])
+                all_objs[i//4]['ymin'] = max(min(ya,yb,yc,yd),0)
+                all_objs[i//4]['ymax'] = min(max(ya,yb,yc,yd),image.shape[0]) 
+              
         # resize the image to standard size
         image = cv2.resize(image, (self.config['IMAGE_W'], self.config['IMAGE_H']))
         if self.config['IMAGE_C'] == 1: image = image[:,:,np.newaxis]
@@ -358,20 +375,12 @@ class BatchGenerator(Sequence):
 
         # fix object's position and size
         for obj in all_objs:
-            for attr in ['xmin', 'xmax']:
-                if jitter: obj[attr] = int(obj[attr] * scale - offx)
-                    
+            for attr in ['xmin', 'xmax']:                    
                 obj[attr] = int(obj[attr] * float(self.config['IMAGE_W']) / w)
                 obj[attr] = max(min(obj[attr], self.config['IMAGE_W']), 0)
                 
-            for attr in ['ymin', 'ymax']:
-                if jitter: obj[attr] = int(obj[attr] * scale - offy)
-                    
+            for attr in ['ymin', 'ymax']:                    
                 obj[attr] = int(obj[attr] * float(self.config['IMAGE_H']) / h)
                 obj[attr] = max(min(obj[attr], self.config['IMAGE_H']), 0)
 
-            if jitter and flip > 0.5:
-                xmin = obj['xmin']
-                obj['xmin'] = self.config['IMAGE_W'] - obj['xmax']
-                obj['xmax'] = self.config['IMAGE_W'] - xmin
         return image, all_objs
